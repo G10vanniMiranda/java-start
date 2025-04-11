ba47e18afd698c5cd3d4cf64b7fd269f1e8c9861
Giovanni Miranda <giovannimiranda09@gmail.com>
2025-04-11 10:52:00 -0400

    first commit

---
+++ src/main/java/com/dev/GiovvaniDev/GiovvaniDevApplication.java
@@ -0,0 +1,13 @@
+package com.dev.GiovvaniDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiovvaniDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiovvaniDevApplication.class, args);
+	}
+
+}
